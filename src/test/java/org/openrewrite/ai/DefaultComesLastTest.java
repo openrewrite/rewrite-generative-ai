58f4cf14d256365cb004eb8ffb8cb180bcff3840
Jonathan Schneider <jkschneider@gmail.com>
2022-12-23 12:09:27 -0500

    Polish

--- src/test/java/org/openrewrite/ai/DefaultComesLastTest.java
+++ src/test/java/org/openrewrite/ai/DefaultComesLastTest.java
@@ -1,13 +1,17 @@
 package org.openrewrite.ai;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.RepeatedTest;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.HttpSenderExecutionContextView;
 import org.openrewrite.InMemoryExecutionContext;
 import org.openrewrite.ai.model.GenerativeCodeExecutionContextView;
+import org.openrewrite.ipc.http.HttpUrlConnectionSender;
 import org.openrewrite.test.RecipeSpec;
 import org.openrewrite.test.RewriteTest;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.time.Duration;
 
 import static java.util.Objects.requireNonNull;
 import static org.openrewrite.java.Assertions.java;
@@ -17,17 +21,21 @@ public class DefaultComesLastTest implements RewriteTest {
     @Override
     public void defaults(RecipeSpec spec) {
         try (InputStream token = requireNonNull(getClass().getResourceAsStream("/token.txt"))) {
+            ExecutionContext ctx = GenerativeCodeExecutionContextView.view(new InMemoryExecutionContext())
+              .setOpenApiToken(new String(token.readAllBytes()));
+            ctx = HttpSenderExecutionContextView.view(ctx).setHttpSender(new HttpUrlConnectionSender(
+              Duration.ofSeconds(10), Duration.ofSeconds(30)));
+
             spec.recipe(new DefaultComesLast())
               .cycles(1)
               .expectedCyclesThatMakeChanges(1)
-              .executionContext(GenerativeCodeExecutionContextView.view(new InMemoryExecutionContext())
-                .setOpenApiToken(new String(token.readAllBytes())));
+              .executionContext(ctx);
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
 
-    @Test
+    @RepeatedTest(10)
     void edit() {
         rewriteRun(
           //language=java
