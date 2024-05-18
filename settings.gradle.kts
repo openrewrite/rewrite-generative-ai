5c200134b761aec568f0dff8f831766e21a4e4c4
Sam Snyder <sam@moderne.io>
2024-05-17 21:58:55 -0700

    Gradle Enterprise -> Develocity

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,28 @@
 rootProject.name = "rewrite-generative-ai"
+
+plugins {
+    id("com.gradle.develocity") version "latest.release"
+    id("com.gradle.common-custom-user-data-gradle-plugin") version "latest.release"
+}
+
+develocity {
+    server = "https://ge.openrewrite.org/"
+
+    val isCiServer = System.getenv("CI")?.equals("true") ?: false
+    val accessKey = System.getenv("GRADLE_ENTERPRISE_ACCESS_KEY")
+    val authenticated = !accessKey.isNullOrBlank()
+    buildCache {
+        remote(develocity.buildCache) {
+            isEnabled = true
+            isPush = isCiServer && authenticated
+        }
+    }
+
+    buildScan {
+        capture {
+            fileFingerprints = true
+        }
+
+        uploadInBackground = !isCiServer
+    }
+}
