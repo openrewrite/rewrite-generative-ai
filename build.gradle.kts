2752a5909b7fcd3a53f5cc2cfbacc327a56891ee
Tim te Beek <timtebeek@gmail.com>
2025-07-05 17:36:58 +0000

    Avoid `-SNAPSHOT` versions of JUnit 5.+
    Use this link to re-run the recipe: https://app.moderne.io/recipes/org.openrewrite.text.FindAndReplace?organizationId=ODQ2MGExMTUtNDg0My00N2EwLTgzMGMtNGE1NGExMTBmZDkw#defaults%3DW3sibmFtZSI6ImZpbmQiLCJ2YWx1ZSI6Imp1bml0LWp1cGl0ZXItZW5naW5lOjUuKyJ9LHsibmFtZSI6InJlcGxhY2UiLCJ2YWx1ZSI6Imp1bml0LWp1cGl0ZXItZW5naW5lOjUuMTMuMyJ9LHsibmFtZSI6ImZpbGVQYXR0ZXJuIiwidmFsdWUiOiIqKi9idWlsZC5ncmFkbGUua3RzIn1d
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
     testImplementation("org.junit.jupiter:junit-jupiter-api:5.+")
     testImplementation("org.junit.jupiter:junit-jupiter-params:5.+")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.+")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.13.3")
 
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.assertj:assertj-core:latest.release")
