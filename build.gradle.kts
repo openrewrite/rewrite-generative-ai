4be215374efc58169ec96d54aa3f3b3c4b65e3f9
Tim te Beek <timtebeek@gmail.com>
2026-05-22 10:29:51 +0000

    Remove explicit test dependencies
    Now handled by https://github.com/openrewrite/rewrite-build-gradle-plugin/pull/175
    
    Use this link to re-run the recipe: https://app.moderne.io/builder/Qzl8kgixb?organizationId=QUxML01vZGVybmUvTW9kZXJuZSArIE9wZW5SZXdyaXRl
    
    Co-authored-by: Moderne <team@moderne.io>

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,10 +23,6 @@ dependencies {
     // Need to have a slf4j binding to see any output enabled from the parser.
     runtimeOnly("ch.qos.logback:logback-classic:1.5.+")
 
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.14.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-params:5.14.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.14.2")
-
     testImplementation("org.openrewrite:rewrite-test")
     testImplementation("org.assertj:assertj-core:latest.release")
 }
